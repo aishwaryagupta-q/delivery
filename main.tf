b224bbef163bbd3d6327d6b9cb7d5db95dffcbb7
aishwaryagupta-q <70533051+aishwaryagupta-q@users.noreply.github.com>
2020-09-01 21:35:59 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  version = "3.5.0"
+
+  project = "training-freshers" //enter project name
+  region  = "us-central1" //enter correct location
+  zone    = "us-central1-c" //enter zone
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "aishwarya-terraform-instance"
+  machine_type = "f1-micro"
+  tags = ["name","aishwarya"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
